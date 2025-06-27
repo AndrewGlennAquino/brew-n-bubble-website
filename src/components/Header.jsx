5738de216e1299b4af7370a672950ffe2821e2dc
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-27 17:56:02 -0500

    Change design and adjust animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,16 +39,18 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-snow shadow sticky top-0 left-0 right-0">
+    <header className="bg-forest/75 fixed z-10 top-0 left-0 right-0">
       <div className="container mp-default flex justify-between items-center">
+        {/* Logo */}
         <motion.h1
-          className="text-4xl"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 1.1 }}
+          className="text-rose text-4xl"
+          whileHover={{ scale: 1.15 }}
+          whileTap={{ scale: 1.15 }}
         >
           <Link to="/">Brew 'n Bubble</Link>
         </motion.h1>
 
+        {/* Hamburger menu stack */}
         <motion.nav
           id="hamburger-nav"
           className="w-12 h-12 flex flex-col justify-center items-center gap-2"
