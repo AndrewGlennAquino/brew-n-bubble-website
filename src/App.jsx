36b8c784517b7d8bf3f1a8e4c06059d445f4cd82
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-27 02:28:47 -0500

    Add header and route to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { motion } from "motion/react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
+
 const App = () => {
-}
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
