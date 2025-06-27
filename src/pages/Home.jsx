317d188e7ba394b6e502a79e18c6d8e98e3729fd
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-27 17:57:07 -0500

    Create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { motion } from "motion/react";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <section id="hero" className="w-dvw h-dvh relative">
+      <Hero />
+    </section>
+  );
+};
+
+export default Home;
