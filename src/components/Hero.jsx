d4de98efdfbc19b22235cdbc721dca401bdf280d
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-27 23:04:58 -0500

    Add fade in animations to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,35 @@
 import { motion } from "motion/react";
+import { Link } from "react-router-dom";
 import tempBobaVideo from "../assets/videos/temp-boba-video.mp4";
 
 const Hero = () => {
+  // Animation variants for the text container
+  const textContainerVariants = {
+    animateFadeIn: {
+      transition: { delayChildren: 0.25, staggerChildren: 0.25 },
+    },
+  };
+
+  // Animation variants for the text
+  const textVariants = {
+    initial: { scale: 0 },
+    animateFadeIn: { 
+      opacity: 1, 
+      scale: 1,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut"
+      }
+    },
+  };
+
   return (
     <div className="container h-screen mp-default">
       {/* Hero background video */}
       <video
         className="w-screen h-screen absolute inset-0 -z-10 object-cover"
         src={tempBobaVideo}
+        aria-label="Temporary Boba Video"
         playsInline
         autoPlay
         muted
@@ -15,19 +37,35 @@ const Hero = () => {
       />
 
       {/* Text container */}
-      <div className="text-rose w-full h-full flex flex-col justify-center gap-8">
-        <p className="rounded-text-bg text-lg md:text-2xl font-bold">
+      <motion.div
+        className="text-rose w-full h-full flex flex-col justify-center gap-4"
+        initial="initial"
+        animate="animateFadeIn"
+        variants={textContainerVariants}
+      >
+        <motion.p
+          className="rounded-text-bg text-lg lg:text-2xl font-bold"
+          variants={textVariants}
+        >
           Welcome to
-        </p>
-        <h1 className="rounded-text-bg text-5xl md:text-7xl">Brew 'n Bubble</h1>
-        <motion.button
-          className="rounded-text-bg bg-rose text-forest text-lg md:text-2xl font-bold"
+        </motion.p>
+
+        <motion.h1
+          className="rounded-text-bg text-5xl lg:text-7xl"
+          variants={textVariants}
+        >
+          Brew 'n Bubble
+        </motion.h1>
+
+        <motion.div
+          className="rounded-text-bg bg-rose text-forest text-lg lg:text-2xl font-bold"
+          variants={textVariants}
           whileHover={{ scale: 1.15 }}
           whileTap={{ scale: 1.15 }}
         >
-          View Our Menu
-        </motion.button>
-      </div>
+          <Link to="#">View Our Menu</Link>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
