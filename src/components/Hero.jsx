142028fba4b997ae379a1174b20aaed26a374c12
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-01 13:34:38 -0500

    Adjust colors and add transparent white background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   // Animation variants for the text container
   const textContainerVariants = {
     animateFadeIn: {
-      transition: { delayChildren: 0.25, staggerChildren: 0.25 },
+      transition: { delayChildren: 0.25, staggerChildren: 0.15 },
     },
   };
 
@@ -14,10 +14,9 @@ const Hero = () => {
   const textVariants = {
     initial: { scale: 0 },
     animateFadeIn: { 
-      opacity: 1, 
       scale: 1,
       transition: {
-        duration: 0.5,
+        duration: 0.25,
         ease: "easeOut"
       }
     },
@@ -36,9 +35,12 @@ const Hero = () => {
         loop
       />
 
+      {/* Transparent background filter */}
+      <div className="bg-snow/15 w-screen h-screen absolute inset-0 -z-10" />
+
       {/* Text container */}
       <motion.div
-        className="text-rose w-full h-full flex flex-col justify-center gap-4"
+        className="text-coffee w-full h-full flex flex-col justify-center gap-4"
         initial="initial"
         animate="animateFadeIn"
         variants={textContainerVariants}
@@ -51,14 +53,14 @@ const Hero = () => {
         </motion.p>
 
         <motion.h1
-          className="rounded-text-bg text-5xl lg:text-7xl"
+          className="rounded-text-bg text-5xl lg:text-6xl"
           variants={textVariants}
         >
           Brew 'n Bubble
         </motion.h1>
 
         <motion.div
-          className="rounded-text-bg bg-rose text-forest text-lg lg:text-2xl font-bold"
+          className="rounded-text-bg bg-tan text-coffee text-lg lg:text-2xl font-bold"
           variants={textVariants}
           whileHover={{ scale: 1.15 }}
           whileTap={{ scale: 1.15 }}
