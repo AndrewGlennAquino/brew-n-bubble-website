707e42d17634a47f7b2b278be01e469459ad7b3b
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-02 18:22:48 -0500

    Add ScrolledContext to track global state

---
+++ src/contexts/ScrolledContext.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// Context that stores if the user scrolled past the scrollbar origin
+const ScrolledContext = createContext();
+
+export default ScrolledContext;
