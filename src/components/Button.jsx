38ecc891d84127052d6ecc09fec0149059a9849d
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-02 18:19:11 -0500

    Add fully customizeable button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,35 @@
+import { motion } from "motion/react";
+
+/**
+ * Fully customizeable button component that takes
+ * style, url, text, ariaLabel, and Motion props as props.
+ *
+ * @param {*} props style, url, text, ariaLabel, initial, animate, variants
+ */
+const Button = ({
+  style,
+  url,
+  text,
+  ariaLabel,
+  initial,
+  animate,
+  variants,
+}) => {
+  return (
+    <motion.a
+      href={url}
+      target="_blank"
+      className={`rounded-text-bg text-lg xl:text-xl font-bold ${style}`}
+      aria-label={ariaLabel}
+      initial={initial}
+      animate={animate}
+      whileHover={{ scale: 1.15 }}
+      whileTap={{ scale: 1.15 }}
+      variants={variants}
+    >
+      {text}
+    </motion.a>
+  );
+};
+
+export default Button;
