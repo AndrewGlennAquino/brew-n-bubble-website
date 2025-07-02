bf8c661d84d4bc50dae75ff82f14669b4cc7f49a
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-02 18:54:09 -0500

    Initial gh-pages commit

--- src/contexts/ScrolledContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-// Context that stores if the user scrolled past the scrollbar origin
-const ScrolledContext = createContext();
-
-export default ScrolledContext;
