c8358c0b9a8c3d268198fb5ba5bc361d385be118
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-08 13:05:28 -0500

    Fix button not scaling on hover and tap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,24 +22,30 @@ const Hero = () => {
       {/* Transparent background filter */}
       <div className="bg-coffee/50 w-screen h-screen absolute inset-0 -z-10" />
 
-      {/* Animated container */}
-      <FadeIn className="text-butter w-full h-full flex flex-col justify-center gap-8">
-        {/* Hero subtext */}
-        <p className="text-xl xl:text-2xl font-bold">Welcome to</p>
+      {/* Text container */}
+      <div className="text-butter w-full h-full flex flex-col justify-center gap-8">
+        <FadeIn className="flex flex-col justify-center gap-8">
+          {/* Hero subtext */}
+          <p className="text-xl xl:text-2xl font-bold leading-none">
+            Welcome to
+          </p>
 
-        {/* Hero header */}
-        <h1 className="font-pacifico text-glow text-5xl xl:text-6xl">
-          Brew 'n Bubble
-        </h1>
+          {/* Hero header */}
+          <h1 className="font-pacifico text-glow text-5xl xl:text-6xl">
+            Brew 'n Bubble
+          </h1>
+        </FadeIn>
 
         {/* View our menu button */}
         <Button
-          style="bg-butter text-night"
+          className="bg-butter text-night"
           url="https://direct.chownow.com/order/34791/locations/52608"
           text="View Our Menu"
-          ariaLabel="View our menu button"
+          aria-label="View our menu button"
+          initial={{ y: 25, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1, transition: { duration: 0.5, delay: 0.75 } }}
         />
-      </FadeIn>
+      </div>
     </section>
   );
 };
