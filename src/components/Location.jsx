6f52321d9a45488f2d6d4fc4d7cf9c92164c76f3
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-10 16:56:49 -0500

    Add animation to location container

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,41 +1,67 @@
-import FadeIn from "./FadeIn";
+import { motion } from "motion/react"; // eslint-disable-line no-unused-vars
 
 /**
  * Location component with animated header and interactive google map
  */
 const Location = () => {
+  // Get Google Maps public API key from environment variable
   const mapsPublicKey = import.meta.env.VITE_GOOGLE_MAPS_API_PUBLIC_KEY;
+
+  // Animation variants for the container that staggers children
+  const parentVariants = {
+    animateFadeIn: {
+      transition: { delayChildren: 0.25, staggerChildren: 0.25 },
+    },
+  };
+
+  // Animation variants for the text and map element to fade in
+  const childrenVariants = {
+    initial: { y: 25, opacity: 0 },
+    animateFadeIn: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    },
+  };
 
   return (
     <section
-      className="h-screen md:h-[55vh] mp-default flex justify-center items-center relative"
+      className="h-full mp-default flex justify-center items-center relative"
       aria-label="Location"
     >
       {/* Animated container */}
-      <FadeIn className="container h-fit flex flex-col sm:flex-row justify-center items-center gap-8">
+      <motion.div 
+        className="container w-full h-full flex flex-col jusity-center"
+        initial="initial"
+        whileInView="animateFadeIn"
+        viewport={{ amount: 0.5 }}
+        variants={parentVariants}
+      >
         {/* Location header */}
-        <div className="bg-snow/25 w-fit p-2 flex flex-col flex-grow-0 basis-full gap-8 rounded-2xl">
+        <motion.div 
+          className="bg-snow/25 p-2 flex flex-col gap-8"
+          variants={childrenVariants}
+        >
           <h1>Location</h1>
-
-          {/* Location text */}
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            placeat tenetur accusantium dolores quidem consequuntur, molestiae
-            sint corrupti nam hic doloremque error fugit fugiat! Eaque unde
-            ipsum ut tenetur laudantium!
-          </p>
-        </div>
+          <p>2415 Bowes Road, STE 108, Elgin, IL</p>
+        </motion.div>
 
         {/* Google Maps embed API */}
-        <div className="w-full sm:w-[40vw] 2xl:w-[30vw] h-[45vh] sm:h-[40vh] 2xl:h-[30vh]">
+        <motion.div 
+          className="w-full h-[55vh]"
+          variants={childrenVariants}
+        >
           <iframe
             className="w-full h-full"
             loading="lazy"
             allowFullScreen
             src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJh3GibEwbD4gRKCCmpnJvNGI&key=${mapsPublicKey}`}
           />
-        </div>
-      </FadeIn>
+        </motion.div>
+      </motion.div>
     </section>
   );
 };
