9195e3a9f652da7bb8c3952b4b621346d31055e8
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-10 20:05:56 -0500

    Remove height and width styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,13 +22,13 @@ const About = () => {
 
   return (
     <section
-      className="h-fit mp-default flex justify-center items-center relative"
+      className="mp-default flex justify-center items-center relative"
       aria-label="About"
     >
       {/* Animated container */}
-      <FadeIn className="container h-fit flex flex-col sm:flex-row justify-center items-center gap-8">
+      <FadeIn className="container flex flex-col sm:flex-row justify-center items-center gap-8">
         {/* About header */}
-        <div className="bg-snow/25 w-fit flex flex-col flex-grow-0 basis-full gap-8 rounded-2xl">
+        <div className="bg-snow/25 flex flex-col flex-grow-0 basis-full gap-8 rounded-2xl">
           <h1>About us</h1>
 
           {/* About text */}
