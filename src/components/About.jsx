40b5a1bb4938de5155776a6f42e443b9e481c045
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-07-14 13:28:55 -0500

    Add background to text container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -54,7 +54,7 @@ const About = () => {
       >
         {/* About header */}
         <motion.div
-          className="bg-snow/25 w-full h-full flex flex-col gap-8 rounded-2xl"
+          className="bg-coffee text-snow w-full h-full p-4 sm:p-8 flex flex-col gap-8 rounded-2xl"
           variants={childrenVariants}
         >
           <h1>About us</h1>
